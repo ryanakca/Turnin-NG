c22f79963a9de083de7b5948045f067960e4f30e
Ryan Kavanagh <ryanakca@kubuntu.org>
2009-07-06 11:31:34 -0400

    Ported to configobj

--- src/project/configparser.py
+++ src/project/configparser.py
@@ -1,48 +1,70 @@
-import ConfigParser
+from configobj import ConfigObj
 
-class TurninGlobalConfig(object):
+class ProjectGlobalConfig(object):
     """ This class is in case we ever decide we want global configurations. """
     
     def __init__(self, config_file):
-        self.config_file = config_file
-        self.config = ConfigParser.SafeConfigParser()
-        self.config.read(self.config_file)
+        self.config = ConfigObj()
+        self.config.filename = config_file
+        self.config.indent_type = '    '
+        self.config.unrepr = True
 
-class TurninCourse(TurninGlobalConfig):
+class ProjectCourse(ProjectGlobalConfig):
     """ This class represents a turnin course object. """
 
     def __init__(self, config_file, course):
-        super(TurninCourse, self).__init__(config_file)
+        super(ProjectCourse, self).__init__(config_file)
         self.course = course
-        if not self.config.has_section(self.course):
-            self.config.add_section(self.course)
-            self.config.write(open(config_file, 'wb'))
+        if not self.config.__dict__.__contains__(course):
+            self.config.reload() # We don't want to clobber something
+            self.config[course] = {}
+            self.config.write()
 
     def read(self):
         """ Reads the self.course section in the config file. """
-        try:
-            self.user = self.config.get(self.course, 'user')
-            self.directory = self.config.get(self.course, 'directory')
-            self.group = self.config.get(self.course, 'group')
-            self.sections = self.config.get(self.course, 'sections').split(';')
-        except ConfigParser.NoSectionError, e:
-            print "Course %s does not exist." % e.section
-        except ConfigParser.NoOptionError, e:
-            print ("Option %s has not yet been defined for the course %s." %
-                (e.option, e.section))
+        super(ProjectCourse, self).read()
+        self.user = self.config[self.course]['user']
+        self.directory = self.config[self.course]['directory']
+        self.group = self.config[self.course]['group']
+        self.sections = self.config[self.course]['sections']
 
 
     def write(self, user='', directory='', group='', sections=''):
         """ Modifies the config file. """
-        try:
-            if user:
-                self.config.set(self.course, 'user', user)
-            if directory:
-                self.config.set(self.course, 'directory', directory)
-            if group:
-                self.config.set(self.course, 'group', group)
-            if sections:
-                self.config.set(self.course, 'sections', sections)
-            self.config.write(open(self.config_file, 'wb'))
-        except ConfigParser.NoSectionError, e:
-            print "Course %s does not exist." % e.section
+        self.config.reload() # We don't want to clobber something
+        if user:
+            self.config[self.course]['user'] = user
+        if directory:
+            self.config[self.course]['directory'] = directory
+        if group:
+            self.config[self.course]['group'] = group
+        if sections:
+            self.config[self.course]['sections'] = sections
+        self.config.write()
+
+
+class ProjectProject(ProjectCourse):
+    """ This class represents a turnin course's project object. """
+
+    def __init__(self, config_file, course, project):
+        super(ProjectProject, self).__init__(config_file, course)
+        self.project = project
+        if not self.config[course].__contains__(project):
+            self.config.reload() # We don't want to clobber something
+            self.config[course][project] = {}
+            self.config[course][project]['enabled'] = False
+            self.config.write()
+
+    def read(self):
+        """ Reads the project from the config file. """
+        self.config.reload()
+        self.description = self.config[self.course][self.project]['description']
+        self.enabled = self.config[self.course][self.project]['enabled']
+
+    def write(self, enabled, description=''):
+        """ Modifies the config file. """
+        self.config.reload() # We don't want to clobber something
+        self.config[self.course][self.project]['enabled'] = enabled
+        if description:
+            self.config[self.course][self.project]['description'] = description
+        self.config.write()
