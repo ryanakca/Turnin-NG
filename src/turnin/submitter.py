2d6b4a761ffe71ce85d4c4efb21a21e7fbdec00c
Ryan Kavanagh <ryanakca@kubuntu.org>
2009-07-07 14:50:40 -0400

    We're now making the tarball a temporary file as well.

--- src/turnin/submitter.py
+++ src/turnin/submitter.py
@@ -20,25 +20,27 @@ def check_group(group):
 def submit_files(course_name, project, files):
     # Maybe we should use pwd.getpwuid(os.getuid())[4].replace(' ', '') ---
     # that's to say, their full name stored in GECOS.
-    tempdir = tempfile.mkdtemp()
+    temparchive = tempfile.NamedTemporaryFile()
     filename =  '%(username)s-%(timestamp)s.tar.gz' % \
         {'username': pwd.getpwuid(os.getuid())[0],
          'timestamp': time.strftime("%Y-%m-%d-%H%M%S")}
-    archive = os.path.join(tempdir, filename )
-    tar = tarfile.open(archive, 'w:gz')
-    tempdir2 = tempfile.mkdtemp()
+    tempdir = tempfile.mkdtemp()
     for file in files:
         if os.path.isdir(file):
-            shutil.copytree(file, os.path.join(tempdir2, file))
+            shutil.copytree(file, os.path.join(tempdir, file))
         else:
-            shutil.copy(file, tempdir2)
-    tar.add(file, '%(projectname)s-%(username)s' %
+            shutil.copy(file, tempdir)
+    os.listdir(tempdir)
+    tar = tarfile.open(temparchive.name, 'w:gz')
+    tar.add(tempdir, '%(projectname)s-%(username)s' %
                            {'projectname': project.name,
                             'username': pwd.getpwuid(os.getuid())[0]})
     submitted_files = tar.members # tar.list(verbose=True)
     tar.close()
-    chown(archive, project.course['user'], project.course['group'])
-    shutil.move(archive, os.path.join(project.project['directory'], filename))
+    chown(temparchive.name, project.course['user'], project.course['group'])
+    shutil.copy(temparchive.name,
+            os.path.join(project.project['directory'], filename))
+    temparchive.close()
     shutil.rmtree(tempdir, ignore_errors=True)
     for j, file in enumerate(submitted_files):
         submitted_files[j] = file.name
