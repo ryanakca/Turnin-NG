cf0d3c86ecfbd30f7af3e7f5b73d6c1c026e82ad
Ryan Kavanagh <ryanakca@kubuntu.org>
2009-07-07 10:16:50 -0400

    We now properly delete the project.

--- src/project/projectmanage.py
+++ src/project/projectmanage.py
@@ -1,5 +1,6 @@
 import os
 import os.path
+import shutil
 
 from project.configparser import ProjectCourse, ProjectProject
 from project.sys import chown
@@ -16,14 +17,15 @@ def create_project(config_file, course, project):
     project_obj.write(True, description)
 
 def delete_project(config_file, course, project):
-    project_obj = ProjectProject(config_file, course, project)
-    if project_obj.course.has_key[project]:
+    if ProjectCourse(config_file, course).course.has_key(project):
+        project_obj = ProjectProject(config_file, course, project)
         certain = raw_input("If you really want to delete this course and " +
                 "all associated files, enter 'yes' in capital letters: ")
         if certain == 'YES':
-            directory = os.path.join(project.course['directory'], project)
+            directory = os.path.join(project_obj.course['directory'], project)
             shutil.rmtree(directory, ignore_errors=True)
             del project_obj.course[project]
+            project_obj.config.write()
         else:
             raise ValueError("Aborting and keeping project.")
     else:
