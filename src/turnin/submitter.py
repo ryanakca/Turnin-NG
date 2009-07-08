9f3fd99eaebfd6ecc6f0e78c83d9ff893e0ca800
Ryan Kavanagh <ryanakca@kubuntu.org>
2009-07-08 08:47:27 -0400

    Added epydoc docstrings to the submitter functions

--- src/turnin/submitter.py
+++ src/turnin/submitter.py
@@ -11,6 +11,15 @@ from turnin.sys import chown
 from turnin.configparser import TurninCourse
 
 def check_group(group):
+    """
+    Checks if the user / runner belongs to the group 'group'.
+
+    @type group: string
+    @param group: UNIX group name
+    @rtype: Bool
+    @return: True if the user belongs to the group, False otherwise.
+
+    """
     # Gets the group database entry, selects [2] from the tuple (gr_name,
     # gr_passwd, gr_gid, gr_mem). Checks if the gid is in the process owner's
     # groups list.
@@ -19,6 +28,19 @@ def check_group(group):
     return False
 
 def submit_files(course_name, project, files):
+    """
+    Submits the files to the project.
+
+    @type course_name: string
+    @param course_name: course name
+    @type project: TurninProject
+    @param project: Project to which we should submit the files
+    @type files: list
+    @param files: Python list of files.
+    @rtype: list
+    @return: Python list of submitted files
+
+    """
     # Maybe we should use pwd.getpwuid(os.getuid())[4].replace(' ', '') ---
     # that's to say, their full name stored in GECOS.
     temparchive = tempfile.NamedTemporaryFile()
@@ -48,6 +70,17 @@ def submit_files(course_name, project, files):
     return submitted_files
 
 def list_projects(config, course):
+    """
+    Lists available projects for the course.
+
+    @type config: string
+    @param config: configuration file
+    @type course: string
+    @param course: course name
+    @rtype: list
+    @return: List of (Enabled/Disabled, Project_name) tuples
+
+    """
     projects = ['(Enabled, Project)']
     course_obj = TurninCourse(config, course)
     for i in course_obj.course.__dict__['sections']:
