55e29e570ddc399566dc2530cea7d38285de05f5
Ryan Kavanagh <ryanakca@kubuntu.org>
2009-07-08 12:34:14 -0400

    Scripts project and turnin have lost their .py extension

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='turnin-ng',
       description='Turn in your assignments with turnin',
       author='Ryan Kavanagh',
       author_email='ryanakca@kubuntu.org',
-      scripts=['src/project.py', 'src/turnin.py'],
+      scripts=['src/bin/project', 'src/bin/turnin'],
       packages=['turnin'],
       package_dir={'turnin':'src/turnin'},
 )
