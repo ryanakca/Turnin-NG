6f3c4ca72574f57aed8450a2aee6f3919ef8be40
Ryan Kavanagh <ryanakca@kubuntu.org>
2009-07-08 15:12:24 -0400

    Releasing version 0.1~beta1, hurray\!

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='turnin-ng',
-      version='0.1_pre-alpha',
+      version='0.1~beta1',
       description='Turn in your assignments with turnin',
       author='Ryan Kavanagh',
       author_email='ryanakca@kubuntu.org',
