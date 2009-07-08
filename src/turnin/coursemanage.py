284be347ceb14eecdfde5f1e50fe0eef7da2471b
Ryan Kavanagh <ryanakca@kubuntu.org>
2009-07-08 08:34:00 -0400

    Added epydoc docstrings to the course manager.

--- src/turnin/coursemanage.py
+++ src/turnin/coursemanage.py
@@ -5,6 +5,15 @@ from turnin.configparser import ProjectGlobal, ProjectCourse
 from turnin.sys import chown
 
 def create_course(config_file, course):
+    """
+    Create the course 'course'.
+
+    @type config_file: string
+    @param config_file: path to the configuration file
+    @type course: string
+    @param course: course name
+
+    """
     course = ProjectCourse(config_file, course)
     user = raw_input("Username [usually your UNIX login]: ")
     directory = raw_input("Full path to project directory: ")
@@ -17,6 +26,17 @@ def create_course(config_file, course):
     course.write(user, directory, group, sections) 
 
 def delete_course(config_file, course):
+    """
+    Delete the course 'course'.
+
+    @type config_file: string
+    @param config_file: path to the configuration file
+    @type course: string
+    @param course: course name
+    @raise ValueError: The user decides enters anything but 'YES at the prompt.
+    @raise ValueError: The course does not exist.
+
+    """
     course_obj = ProjectCourse(config_file, course)
     if course_obj.config.has_key(course):
         certain = raw_input("If you really want to delete this course and all " +
@@ -34,5 +54,14 @@ def delete_course(config_file, course):
         raise ValueError("%s is not an existing course" % course)
 
 def switch_course(config_file, course):
+    """
+    Sets the default course in the configuration file.
+
+    @type config_file: string
+    @param config_file: path to the configuration file
+    @type course: string
+    @param course: course name
+
+    """
     global_obj = ProjectGlobal(config_file)
     global_obj.set_default(course)
