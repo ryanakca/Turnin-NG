57fb4ad80eb0a24a9d266d7042517dfb1cde8b4c
Ryan Kavanagh <ryanakca@kubuntu.org>
2009-07-08 15:11:54 -0400

    We're now installing our manpages

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ setup(name='turnin-ng',
       description='Turn in your assignments with turnin',
       author='Ryan Kavanagh',
       author_email='ryanakca@kubuntu.org',
+      license='GNU General Public License version 2, or (at your option) ' +\
+              'any later version',
       scripts=['src/bin/project', 'src/bin/turnin'],
       packages=['turnin'],
       package_dir={'turnin':'src/turnin'},
+      data_files=[('/usr/local/man1/', ['doc/turnin.1', 'doc/project.1'])]
 )
