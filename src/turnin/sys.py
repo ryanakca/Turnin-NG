cf9c131cf9ca11e6c260d3c3d82feba0b6817a96
Ryan Kavanagh <ryanakca@kubuntu.org>
2009-07-08 08:49:36 -0400

    Added epydoc docstrings to the chown function

--- src/turnin/sys.py
+++ src/turnin/sys.py
@@ -3,4 +3,17 @@ import grp
 import os
 
 def chown(path, user, group):
+    """
+    Change the owner and the group of 'path'
+
+    @type path: string
+    @param path: file / directory for which to change the owner/group
+    @type user: string
+    @param user: username
+    @type group: string
+    @param group: groupname
+    @rtype: function
+    @return: Function to change the owner and group of 'path'
+
+    """
     return os.chown(path, pwd.getpwnam(user)[2], grp.getgrnam(group)[2])
