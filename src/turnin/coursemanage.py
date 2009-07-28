f8966e21098bcbede777720a94e3caceb8c6779f
Ryan Kavanagh <ryanakca@kubuntu.org>
2009-07-28 11:27:46 -0400

    We need to check that the course actually exists before archiving it, that means, create open the Global config first. By opening the Course config first, we were creating the course, thus the check that it existed failed.

--- src/turnin/coursemanage.py
+++ src/turnin/coursemanage.py
@@ -82,8 +82,9 @@ def delete_course(config_file, course):
     @raise ValueError: The course does not exist.
 
     """
-    course_obj = ProjectAdminCourse(config_file, course)
-    if course_obj.config.has_key(course):
+    config_obj = ProjectGlobal(config_file)
+    if config_obj.config.has_key(course):
+        course_obj = ProjectAdminCourse(config_file, course)
         if raw_input("If you really want to delete this course and all " +
                 "associated files, enter 'yes' in capital letters: ") == 'YES':
             shutil.rmtree(course_obj.course['directory'], ignore_errors=True)
