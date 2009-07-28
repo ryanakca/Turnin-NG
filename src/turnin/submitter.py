c0a1f30668662bffa07cbf752b86af923802cefb
Ryan Kavanagh <ryanakca@kubuntu.org>
2009-07-28 15:39:43 -0400

    Resubmitting now works with UUIDs identifying the course and a random 16 byte suffix on the tarballs.

--- src/turnin/submitter.py
+++ src/turnin/submitter.py
@@ -27,7 +27,7 @@ import tarfile
 import tempfile
 
 from turnin.sys import chown, chgrp
-from turnin.configparser import TurninCourse
+from turnin.configparser import TurninCourse, TurninList
 
 def check_group(group):
     """
@@ -46,7 +46,7 @@ def check_group(group):
         return True
     return False
 
-def submit_files(course_name, project, files, gpg_key=''):
+def submit_files(course_name, project, files, list='', gpg_key=''):
     """
     Submits the files to the project.
 
@@ -56,6 +56,8 @@ def submit_files(course_name, project, files, gpg_key=''):
     @param project: Project to which we should submit the files
     @type files: list
     @param files: Python list of files.
+    @type list: TurninList
+    @param list: list of submitted files
     @type gpg_key: string
     @param gpg_key: GnuPG public-key ID
     @rtype: list
@@ -63,10 +65,24 @@ def submit_files(course_name, project, files, gpg_key=''):
     @raise subprocess.CalledProcessError: GnuPG fails to sign
 
     """
-    chars = string.letters + string.digits
+    if not list:
+        list = os.path.join(os.path.expanduser('~'), '.turnin-ng',
+                'submissions')
+        # We don't want something like /home/ryan/.turning-ng/submissions../
+        if not os.path.isdir(os.path.normpath(os.path.join(list, os.pardir))):
+            os.makedirs(os.path.normpath(os.path.join(list, os.pardir)))
+    list_file = TurninList(list)
     random_suffix = ''
-    for i in range(17):
-        random_suffix += random.choice(chars)
+    if list_file.config.has_key(project.course.name):
+        if list_file.config[project.course.name].has_key(
+                project.project['uuid']):
+            random_suffix = list_file.config[project.course.name][
+                    project.project['uuid']]
+    if not random_suffix:
+        chars = string.letters + string.digits
+        for i in range(15):
+            random_suffix += random.choice(chars)
+        list_file.write(project, random_suffix)
     temparchive = tempfile.NamedTemporaryFile(suffix='.tar.gz')
     filename =  '%(username)s-%(suffix)s.tar.gz' % \
         {'username': pwd.getpwuid(os.getuid())[0], # This is the username that
